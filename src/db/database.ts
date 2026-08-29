f2d8b235ed4bff9b136427eff2853de5fdcec62d
Tino <121829394+faffietheripper@users.noreply.github.com>
2026-08-29 15:03:07 +0100

    register client sync schema

--- src/db/database.ts
+++ src/db/database.ts
@@ -3,6 +3,7 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
 import * as carbonSchema from "./carbon-schema";
+import * as clientSyncSchema from "./client-sync-schema";
 import * as commercialSchema from "./commercial-schema";
 import * as coreSchema from "./schema";
 import * as returnsSchema from "./returns-schema";
@@ -18,6 +19,7 @@ const schema = {
   ...commercialSchema,
   ...returnsSchema,
   ...carbonSchema,
+  ...clientSyncSchema,
 };
 
 declare global {
